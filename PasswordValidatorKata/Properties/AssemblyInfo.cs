69ea145766a5d211aed9995c3ac7815b666c01a7
Maria Soria <maria.it3@gmail.com>
2023-12-01 16:14:23 +0000

    Rename project PasswordValidatorKata

--- PasswordValidatorKata/Properties/AssemblyInfo.cs
+++ PasswordValidatorKata/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasswordValidator")]
+[assembly: AssemblyTitle("PasswordValidatorKata")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PasswordValidator")]
+[assembly: AssemblyProduct("PasswordValidatorKata")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
