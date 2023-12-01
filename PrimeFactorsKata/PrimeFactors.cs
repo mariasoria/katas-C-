3b1500fcc40cc0b556e20a4699f111ee5cbb67b9
Maria Soria <maria.it3@gmail.com>
2023-12-01 15:46:21 +0000

    Add PrimeFactorsKata

---
+++ PrimeFactorsKata/PrimeFactors.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace PrimFactorsKata
+{
+    public class PrimeFactors
+    {
+        public static List<int> generate(int number)
+        {
+            var primes = new List<int>();
+            var primeOption = 2;
+            while (hasStillPrimeFactors(number))
+            {
+                while (aPrimeFactorIsFound(number, primeOption))
+                {
+                    primes.Add(primeOption);
+                    number /= primeOption;
+                }
+
+                primeOption++;
+            }
+            return primes;
+        }
+
+        private static bool aPrimeFactorIsFound(int number, int primeOption)
+        {
+            return number % primeOption == 0;
+        }
+
+        private static bool hasStillPrimeFactors(int number)
+        {
+            return number > 1;
+        }
+    }
+}
+
---
+++ PrimeFactorsKata/PrimeFactorsTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace PrimFactorsKata
+{
+    public class PrimeFactorsTests
+    {
+        [Fact]
+        public void should_return_an_empty_list_if_it_has_no_primer_factors()
+        {
+            PrimeFactors.generate(1).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void should_return_the_input_number_if_it_has_only_one_prime_factor()
+        {
+            PrimeFactors.generate(2).Should().BeEquivalentTo(new List<int> { 2 });
+            PrimeFactors.generate(3).Should().BeEquivalentTo(new List<int> { 3 });
+        }
+
+        [Fact]
+        public void should_return_the_same_prime_factors_of_an_input_number()
+        {
+            PrimeFactors.generate(4).Should().BeEquivalentTo(new List<int> { 2, 2 });
+            PrimeFactors.generate(8).Should().BeEquivalentTo(new List<int> { 2, 2, 2 });
+            PrimeFactors.generate(9).Should().BeEquivalentTo(new List<int> { 3, 3 });
+            PrimeFactors.generate(27).Should().BeEquivalentTo(new List<int> { 3, 3, 3 });
+        }
+
+        [Fact]
+        public void should_find_different_prime_factors_of_an_input_number()
+        {
+            PrimeFactors.generate(6).Should().BeEquivalentTo(new List<int> { 2, 3 });
+            PrimeFactors.generate(12).Should().BeEquivalentTo(new List<int> { 2, 2, 3 });
+        }
+    }
+}
